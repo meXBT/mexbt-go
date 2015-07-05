e50b811984d095479e872673b3bb505ba36e19da
Alexey Derlaft <derlafff@ya.ru>
2015-07-05 21:18:17 +0300

    gofmt && commenting code && new tests

--- mexbt_test.go
+++ mexbt_test.go
@@ -1,9 +1,9 @@
 package mexbt
 
 import (
-    "testing"
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+	"testing"
 )
 
 // Sanity check suite
@@ -12,80 +12,84 @@ import (
 // Sandbox keys
 // Use my 1000 bitcoins as you wish
 var config = Config{
-            ApiKey: "2f0873b7875e1bcdef5677ffcf600dc4",
-            PrivateKey: "fd836ed10efbdd9617a7223e59b2fd8c",
-            UserId: "der@2-47.ru",
+	ApiKey:     "2f0873b7875e1bcdef5677ffcf600dc4",
+	PrivateKey: "fd836ed10efbdd9617a7223e59b2fd8c",
+	UserId:     "der@2-47.ru",
 }
 
 func TestMain(m *testing.M) {
-    Sandbox = true
-    os.Exit(m.Run())
+	Sandbox = true
+	os.Exit(m.Run())
 }
 
 func check(t *testing.T, ok bool, err error) {
-    if err != nil || !ok {
-        fmt.Printf("%+v\n", t)
-        t.Fail()
-    } 
+	if err != nil || !ok {
+		fmt.Printf("%+v\n", t)
+		t.Fail()
+	}
 }
 
 func TestTicker(t *testing.T) {
 
-    res, e := Ticker(BTCMXN)
-    check(t, res.IsAccepted, e)
+	res, e := Ticker(BTCMXN)
+	check(t, res.IsAccepted, e)
 }
 
 func TestTradesByDate(t *testing.T) {
 
-    res, e := TradesByDate(BTCMXN, 141530012, 1416559390)
-    check(t, res.IsAccepted, e)
+	res, e := TradesByDate(BTCMXN, 141530012, 1416559390)
+	check(t, res.IsAccepted, e)
 }
 
 func TestTrades(t *testing.T) {
-    res, e := Trades("BTCMXN", -1, 10)
-    check(t, res.IsAccepted, e)
+	res, e := Trades(BTCMXN, -1, 10)
+	check(t, res.IsAccepted, e)
 }
 
 func TestMe(t *testing.T) {
+	r, e := config.Me()
+	check(t, r.IsAccepted, e)
+}
 
-    r, e := config.Me()
-    check(t, r.IsAccepted, e)
+func TestBalance(t *testing.T) {
+	r, e := config.Balance()
+	check(t, r.IsAccepted, e)
 }
 
 func TestAccountTrades(t *testing.T) {
-
-    r, e := config.AccountTrades(BTCUSD, -1, 20)
-    check(t, r.IsAccepted, e)
+	r, e := config.AccountTrades(BTCUSD, -1, 20)
+	check(t, r.IsAccepted, e)
 }
 
 func TestDepositAdresses(t *testing.T) {
-
-    r, e := config.DepositAddresses()
-    check(t, r.IsAccepted, e)
+	r, e := config.DepositAddresses()
+	check(t, r.IsAccepted, e)
 }
 
 func TestAccountOrders(t *testing.T) {
-
-    r, e := config.AccountOrders()
-    check(t, r.IsAccepted, e)
+	r, e := config.AccountOrders()
+	check(t, r.IsAccepted, e)
 }
 
-
 func TestProductPairs(t *testing.T) {
+	r, e := ProductPairs()
+	check(t, r.IsAccepted, e)
+}
 
-    r, e := ProductPairs()
-    check(t, r.IsAccepted, e)
+func TestOrderbook(t *testing.T) {
+	r, e := Orderbook(BTCUSD)
+	check(t, r.IsAccepted, e)
 }
 
 func TestCreateModifyCancel(t *testing.T) {
-    r, e := config.CreateLimitOrder(BTCMXN, SIDE_BUY, 12.42, 2.02)
-    check(t, r.IsAccepted, e)
-    orderId := r.ServerOrderId
+	r, e := config.CreateLimitOrder(BTCMXN, SIDE_BUY, 12.42, 2.02)
+	check(t, r.IsAccepted, e)
+	orderId := r.ServerOrderId
 
-    r2, e := config.MoveToTop(BTCMXN, orderId)
-    check(t, r2.IsAccepted, e)
-    orderId = r2.ServerOrderId
+	r2, e := config.MoveToTop(BTCMXN, orderId)
+	check(t, r2.IsAccepted, e)
+	orderId = r2.ServerOrderId
 
-    r3, e := config.Cancel(BTCMXN, orderId)
-    check(t, r3.IsAccepted, e)
+	r3, e := config.Cancel(BTCMXN, orderId)
+	check(t, r3.IsAccepted, e)
 }
